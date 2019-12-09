6dc314ddb59961cd76ecd10729a78e9b1b5284a3
Stephen Rowles <stephen.rowles@infosum.com>
2019-12-09 09:03:11 +0000

    day 9 part 2

---
+++ day09_2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/srowles/advent2019"
+)
+
+func main() {
+	computer, err := advent2019.CreateIntcodeComputerFromFile("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to create comuter with error: %v", err)
+	}
+
+	computer.Input(2)
+	for {
+		err = computer.Run()
+		if err == advent2019.ErrHalted {
+			break
+		} else {
+			if err != nil {
+				log.Fatalf("unexpected error:%v", err)
+			}
+		}
+		fmt.Println(computer.Output())
+	}
+}
