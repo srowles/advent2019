f9b861ac3e6f22405d7a0abecb953f97714260f2
Stephen Rowles <stephen@rowles.org.uk>
2019-12-15 23:23:32 +0000

    day15 part 1, manual

---
+++ day15_1/computer.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+var (
+	// ErrHalted means the Computer has halted
+	ErrHalted = errors.New("Halted")
+)
+
+// IntcodeComputer implements an AOC 2019 intcode computer
+type IntcodeComputer struct {
+	pointer      int
+	program      []int
+	Input        chan int
+	inputIdx     int
+	output       int
+	relativeBase int
+}
+
+// Incode Computer Instructions
+var (
+	Add    = 1
+	Mul    = 2
+	Input  = 3
+	Output = 4
+	JIT    = 5
+	JIF    = 6
+	LT     = 7
+	EQ     = 8
+	ARB    = 9
+	Halt   = 99
+)
+
+// Run runs the program until it halts
+func (i *IntcodeComputer) Run() error {
+	for {
+		var codes []int
+		codes = intToSlice(i.program[i.pointer], codes)
+		for k := 0; k <= 3; k++ {
+			codes = append(codes, 0)
+		}
+
+		opcode := codes[0]
+		opcode += codes[1] * 10
+
+		switch opcode {
+		case Add:
+			val := i.getVal(1, codes) + i.getVal(2, codes)
+			if codes[4] == 2 {
+				i.program[i.program[i.pointer+3]+i.relativeBase] = val
+			} else {
+				i.program[i.program[i.pointer+3]] = val
+			}
+			i.pointer += 4
+		case Mul:
+			val := i.getVal(1, codes) * i.getVal(2, codes)
+			if codes[4] == 2 {
+				i.program[i.program[i.pointer+3]+i.relativeBase] = val
+			} else {
+				i.program[i.program[i.pointer+3]] = val
+			}
+			i.pointer += 4
+		case Input:
+			input := <-i.Input
+			if codes[2] == 0 {
+				i.program[i.program[i.pointer+1]] = input
+			} else if codes[2] == 2 {
+				i.program[i.relativeBase+i.program[i.pointer+1]] = input
+			} else {
+				i.program[i.pointer+1] = input
+			}
+			i.inputIdx++
+			i.pointer += 2
+		case Output:
+			if codes[2] == 0 {
+				i.output = i.program[i.program[i.pointer+1]]
+				i.pointer += 2
+				return nil
+			} else if codes[2] == 2 {
+				i.output = i.program[i.relativeBase+i.program[i.pointer+1]]
+				i.pointer += 2
+				return nil
+			} else {
+				i.output = i.program[i.pointer+1]
+				i.pointer += 2
+				return nil
+			}
+		case JIT:
+			first := i.getVal(1, codes)
+			second := i.getVal(2, codes)
+
+			if first != 0 {
+				i.pointer = second
+			} else {
+				i.pointer += 3
+			}
+		case JIF:
+			first := i.getVal(1, codes)
+			second := i.getVal(2, codes)
+
+			if first == 0 {
+				i.pointer = second
+			} else {
+				i.pointer += 3
+			}
+		case LT:
+			first := i.getVal(1, codes)
+			second := i.getVal(2, codes)
+
+			var val int
+			if first < second {
+				val = 1
+			} else {
+				val = 0
+			}
+			if codes[4] == 2 {
+				i.program[i.program[i.pointer+3]+i.relativeBase] = val
+			} else {
+				i.program[i.program[i.pointer+3]] = val
+			}
+			i.pointer += 4
+		case EQ:
+			first := i.getVal(1, codes)
+			second := i.getVal(2, codes)
+
+			var val int
+			if first == second {
+				val = 1
+			} else {
+				val = 0
+			}
+			if codes[4] == 2 {
+				i.program[i.program[i.pointer+3]+i.relativeBase] = val
+			} else {
+				i.program[i.program[i.pointer+3]] = val
+			}
+			i.pointer += 4
+		case ARB:
+			i.relativeBase += i.getVal(1, codes)
+			i.pointer += 2
+		case Halt:
+			return ErrHalted
+		default:
+			return fmt.Errorf("Invalid opcode: %v found at pointer: %v", i.program[i.pointer], i.pointer)
+		}
+	}
+}
+
+func (i *IntcodeComputer) getVal(idx int, codes []int) int {
+	if codes[idx+1] == 0 {
+		return i.program[i.program[i.pointer+idx]]
+	} else if codes[idx+1] == 2 {
+		return i.program[i.program[i.pointer+idx]+i.relativeBase]
+	}
+	return i.program[i.pointer+idx]
+}
+
+func intToSlice(n int, sequence []int) []int {
+	if n != 0 {
+		i := n % 10
+		sequence = append(sequence, i)
+		return intToSlice(n/10, sequence)
+	}
+	return sequence
+}
+
+// Peek allows inspecting the program
+func (i *IntcodeComputer) Peek(pos int) int {
+	return i.program[pos]
+}
+
+// Poke allows inspecting the program
+func (i *IntcodeComputer) Poke(pos, val int) {
+	i.program[pos] = val
+}
+
+// Output returns the current output value
+func (i *IntcodeComputer) Output() int {
+	return i.output
+}
+
+// CreateIntcodeComputerFromFile creates an intcode computer from the supplied file
+func CreateIntcodeComputerFromFile(fileName string) (*IntcodeComputer, error) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read file '%s' with error: %v", fileName, err)
+	}
+
+	intStrs := strings.Split(strings.TrimSpace(string(data)), ",")
+
+	ints := make([]int, 1000000)
+	for idx, i := range intStrs {
+		val, err := strconv.Atoi(i)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to parse '%v' as an int: %v", i, err)
+		}
+		ints[idx] = val
+	}
+
+	return createIntcodeComputer(ints)
+}
+
+// CreateIntcodeComputer creates an intcode computer from the supplied file
+func createIntcodeComputer(ints []int) (*IntcodeComputer, error) {
+	computer := &IntcodeComputer{
+		program: ints,
+	}
+
+	return computer, nil
+}
---
+++ day15_1/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+
+	"github.com/srowles/advent2019"
+)
+
+var (
+	area = make(map[coord]int)
+	pos  = coord{x: 24, y: 24}
+	dir  = 1
+)
+
+type coord struct {
+	x, y int
+}
+
+func main() {
+	computer, err := advent2019.CreateIntcodeComputerFromFile("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to create comuter with error: %v", err)
+	}
+
+	for {
+		computer.Input(dir)
+		err = computer.Run()
+		if err == advent2019.ErrHalted {
+			break
+		} else {
+			if err != nil {
+				log.Fatalf("unexpected error: %v", err)
+			}
+		}
+		out := computer.Output()
+		if out == 0 {
+			markWall()
+		}
+		if out == 1 {
+			move()
+		}
+		if out == 2 {
+			move()
+			markOxygen()
+		}
+		fmt.Println(pos)
+		fmt.Printf("minx:%d,miny:%d,maxx:%d,maxy:%d\n", minx(), miny(), maxx(), maxy())
+		render()
+
+		r := bufio.NewReader(os.Stdin)
+		c, err := r.ReadByte()
+		if err != nil {
+			panic("wtf" + err.Error())
+		}
+		switch c {
+		case 'w':
+			dir = 1
+		case 's':
+			dir = 2
+		case 'a':
+			dir = 3
+		case 'd':
+			dir = 4
+		}
+	}
+}
+
+func markOxygen() {
+	area[pos] = 2
+}
+
+func move() {
+	switch dir {
+	case 1:
+		pos = coord{x: pos.x, y: pos.y - 1}
+	case 2:
+		pos = coord{x: pos.x, y: pos.y + 1}
+	case 3:
+		pos = coord{x: pos.x - 1, y: pos.y}
+	case 4:
+		pos = coord{x: pos.x + 1, y: pos.y}
+	}
+}
+
+func markWall() {
+	var loc coord
+	switch dir {
+	case 1:
+		loc = coord{x: pos.x, y: pos.y - 1}
+	case 2:
+		loc = coord{x: pos.x, y: pos.y + 1}
+	case 3:
+		loc = coord{x: pos.x - 1, y: pos.y}
+	case 4:
+		loc = coord{x: pos.x + 1, y: pos.y}
+	}
+	area[loc] = 1
+}
+
+func render() {
+	for y := miny() - 5; y < maxy()+5; y++ {
+		for x := minx() - 5; x < maxx()+5; x++ {
+			fmt.Print(charAt(x, y))
+		}
+		fmt.Println()
+	}
+}
+
+func miny() int {
+	m := math.MaxInt64
+	for l := range area {
+		if l.y < m {
+			m = l.y
+		}
+	}
+
+	if pos.y < m {
+		m = pos.y
+	}
+	return m
+}
+
+func minx() int {
+	m := math.MaxInt64
+	for l := range area {
+		if l.x < m {
+			m = l.x
+		}
+	}
+	if pos.x < m {
+		m = pos.x
+	}
+	return m
+}
+
+func maxy() int {
+	m := 0
+	for l := range area {
+		if l.y > m {
+			m = l.y
+		}
+	}
+	if pos.y > m {
+		m = pos.y
+	}
+	return m
+}
+
+func maxx() int {
+	m := 0
+	for l := range area {
+		if l.x > m {
+			m = l.x
+		}
+	}
+	if pos.x > m {
+		m = pos.x
+	}
+	return m
+}
+
+func charAt(x, y int) string {
+	if x == pos.x && y == pos.y {
+		return "D"
+	}
+	if area[coord{x: x, y: y}] == 1 {
+		return "#"
+	}
+	if area[coord{x: x, y: y}] == 2 {
+		return "O"
+	}
+
+	return "."
+}
