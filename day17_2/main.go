7ab73e9806371adf0d97671bf426b2131b3c74f3
Stephen Rowles <stephen.rowles@infosum.com>
2019-12-18 16:58:27 +0000

    day 17 part 2

--- day17_2/main.go
+++ day17_2/main.go
@@ -15,29 +15,35 @@ func main() {
 	}
 
 	computer.Poke(0, 2)
-	computer.Inputs(commandsToInts("A,B"))                 //main
-	computer.Inputs(commandsToInts("R,6"))                 //A
-	computer.Inputs(commandsToInts("L,6,L,6,4,L,6,2,L,6")) //B
-	computer.Inputs(commandsToInts("2"))                   //C
-	computer.Inputs(commandsToInts("y"))                   // video output
+	computer.Inputs(commandsToInts("A,B,A,B,C,A,B,C,A,C")) //main
+	computer.Inputs(commandsToInts("R,6,L,6,L,10"))        //A
+	computer.Inputs(commandsToInts("L,8,L,6,L,10,L,6"))    //B
+	computer.Inputs(commandsToInts("R,6,L,8,L,10,R,6"))    //C
+	computer.Inputs(commandsToInts("n"))                   // video output
+	var lastoutput int
 	for {
 		err = computer.Run()
 		if err == advent2019.ErrHalted {
+			fmt.Println("halted")
 			break
 		} else {
 			if err != nil {
 				log.Fatalf("unexpected error:%v", err)
 			}
 		}
-		fmt.Printf("%s", string(byte(computer.Output())))
+		lastoutput = computer.Output()
+		fmt.Printf("%s", string(byte(lastoutput)))
 	}
+	fmt.Println("\n", lastoutput)
 }
 
 func commandsToInts(in string) []int {
 	a := strings.Split(in, ",")
 	var out []int
 	for _, i := range a {
-		out = append(out, int(i[0]))
+		for _, c := range i {
+			out = append(out, int(c))
+		}
 		out = append(out, 44)
 	}
 	out[len(out)-1] = 10
