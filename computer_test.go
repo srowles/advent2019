6ee5cfa5ad42faa60bb51426150762a0c6da3d0a
Stephen Rowles <stephen.rowles@infosum.com>
2019-12-19 14:01:12 +0000

    day19 part 1

--- computer_test.go
+++ computer_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestCompExample(t *testing.T) {
 	require := require.New(t)
-	c, err := createIntcodeComputer([]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50})
+	c, err := CreateIntcodeComputer([]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50})
 	require.NoError(err)
 	err = c.Run()
 	require.NoError(err)
